3ef6482b0286094e31db4ca70d2bf979b02e3049
Liam_Pankow <zzrhydonzz1@gmail.com>
2020-09-21 11:49:25 -0400

    Added show password functionality to form

--- Cita450Project/Form1.Designer.cs
+++ Cita450Project/Form1.Designer.cs
@@ -34,15 +34,21 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.EnterBtn = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
             this.ClearBtn = new System.Windows.Forms.Button();
             this.BackBtn = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.passwordTxtBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PasswordChk = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(201, 136);
+            this.textBox1.Location = new System.Drawing.Point(126, 146);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
@@ -50,7 +56,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(115, 139);
+            this.label1.Location = new System.Drawing.Point(40, 149);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(63, 13);
             this.label1.TabIndex = 2;
@@ -59,7 +65,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(115, 165);
+            this.label2.Location = new System.Drawing.Point(40, 175);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(64, 13);
             this.label2.TabIndex = 3;
@@ -67,7 +73,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(201, 162);
+            this.textBox2.Location = new System.Drawing.Point(126, 172);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 4;
@@ -75,7 +81,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(117, 95);
+            this.label3.Location = new System.Drawing.Point(61, 106);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(154, 13);
             this.label3.TabIndex = 5;
@@ -83,26 +89,17 @@
             // 
             // EnterBtn
             // 
-            this.EnterBtn.Location = new System.Drawing.Point(120, 248);
+            this.EnterBtn.Location = new System.Drawing.Point(14, 326);
             this.EnterBtn.Name = "EnterBtn";
             this.EnterBtn.Size = new System.Drawing.Size(75, 23);
             this.EnterBtn.TabIndex = 6;
-            this.EnterBtn.Text = "Enter";
+            this.EnterBtn.Text = "Save";
             this.EnterBtn.UseVisualStyleBackColor = true;
             this.EnterBtn.Click += new System.EventHandler(this.EnterBtn_Click);
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(101, 209);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(213, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "An ID number will be automatically assigned";
-            // 
             // ClearBtn
             // 
-            this.ClearBtn.Location = new System.Drawing.Point(216, 248);
+            this.ClearBtn.Location = new System.Drawing.Point(95, 326);
             this.ClearBtn.Name = "ClearBtn";
             this.ClearBtn.Size = new System.Drawing.Size(75, 23);
             this.ClearBtn.TabIndex = 8;
@@ -112,31 +109,98 @@
             // 
             // BackBtn
             // 
-            this.BackBtn.Location = new System.Drawing.Point(361, 12);
+            this.BackBtn.Location = new System.Drawing.Point(176, 326);
             this.BackBtn.Name = "BackBtn";
-            this.BackBtn.Size = new System.Drawing.Size(43, 23);
+            this.BackBtn.Size = new System.Drawing.Size(75, 23);
             this.BackBtn.TabIndex = 10;
-            this.BackBtn.Text = "back";
+            this.BackBtn.Text = "Cancel";
             this.BackBtn.UseVisualStyleBackColor = true;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(148, 48);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(91, 30);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(100, 13);
+            this.label5.Size = new System.Drawing.Size(167, 20);
             this.label5.TabIndex = 11;
             this.label5.Text = "Add User Info Page";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(126, 224);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Email: ";
+            // 
+            // passwordTxtBox
+            // 
+            this.passwordTxtBox.Location = new System.Drawing.Point(126, 250);
+            this.passwordTxtBox.Name = "passwordTxtBox";
+            this.passwordTxtBox.PasswordChar = '*';
+            this.passwordTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordTxtBox.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(40, 253);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Password: ";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(126, 198);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Username: ";
+            // 
+            // PasswordChk
+            // 
+            this.PasswordChk.AutoSize = true;
+            this.PasswordChk.Location = new System.Drawing.Point(232, 253);
+            this.PasswordChk.Name = "PasswordChk";
+            this.PasswordChk.Size = new System.Drawing.Size(102, 17);
+            this.PasswordChk.TabIndex = 18;
+            this.PasswordChk.Text = "Show Password";
+            this.PasswordChk.UseVisualStyleBackColor = true;
+            this.PasswordChk.CheckedChanged += new System.EventHandler(this.PasswordChk_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(416, 326);
+            this.ClientSize = new System.Drawing.Size(349, 361);
+            this.Controls.Add(this.PasswordChk);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.passwordTxtBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.BackBtn);
             this.Controls.Add(this.ClearBtn);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.EnterBtn);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
@@ -157,10 +221,16 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button EnterBtn;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button ClearBtn;
         private System.Windows.Forms.Button BackBtn;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox passwordTxtBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox PasswordChk;
     }
 }
 
--- Cita450Project/Form1.cs
+++ Cita450Project/Form1.cs
@@ -12,6 +12,7 @@ namespace Cita450Project
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
@@ -30,6 +31,27 @@ namespace Cita450Project
         private void button3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void PasswordChk_CheckedChanged(object sender, EventArgs e)
+        {
+            //grab text box property password char
+            char passwordChar = passwordTxtBox.PasswordChar;
+            //convert "*" to char 
+            string str = "*";
+            char character = char.Parse(str);
+            //compare passwordChar to character
+            if (passwordChar == character)
+            {
+                //if yes, change to ""
+                passwordTxtBox.PasswordChar = '\u0000';
+            }
+
+            else
+            {
+                //else remain "*"
+                passwordTxtBox.PasswordChar = character;
+            }
         }
     }
 }
