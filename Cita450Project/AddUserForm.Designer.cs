9d9b85c7e7007c4b12f1f6cc1428eff492bb1a36
Liam_Pankow <zzrhydonzz1@gmail.com>
2020-09-23 10:48:14 -0400

    rename form

---
+++ Cita450Project/AddUserForm.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Cita450Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fNameTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lNameTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.EnterBtn = new System.Windows.Forms.Button();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.emailTxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.passwordTxtBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.userNameTxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PasswordChk = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.DataGrid1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGrid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fNameTxt
+            // 
+            this.fNameTxt.Location = new System.Drawing.Point(81, 22);
+            this.fNameTxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.fNameTxt.Name = "fNameTxt";
+            this.fNameTxt.Size = new System.Drawing.Size(111, 20);
+            this.fNameTxt.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "First Name: ";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(14, 49);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Last Name: ";
+            // 
+            // lNameTxt
+            // 
+            this.lNameTxt.Location = new System.Drawing.Point(81, 46);
+            this.lNameTxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lNameTxt.Name = "lNameTxt";
+            this.lNameTxt.Size = new System.Drawing.Size(111, 20);
+            this.lNameTxt.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 58);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Please Enter Information Below";
+            // 
+            // EnterBtn
+            // 
+            this.EnterBtn.Location = new System.Drawing.Point(5, 271);
+            this.EnterBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.EnterBtn.Name = "EnterBtn";
+            this.EnterBtn.Size = new System.Drawing.Size(75, 23);
+            this.EnterBtn.TabIndex = 7;
+            this.EnterBtn.Text = "Save";
+            this.EnterBtn.UseVisualStyleBackColor = true;
+            this.EnterBtn.Click += new System.EventHandler(this.EnterBtn_Click);
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.Location = new System.Drawing.Point(84, 271);
+            this.ClearBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(75, 23);
+            this.ClearBtn.TabIndex = 8;
+            this.ClearBtn.Text = "Clear";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.Location = new System.Drawing.Point(163, 271);
+            this.CancelBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.CancelBtn.TabIndex = 9;
+            this.CancelBtn.Text = "Cancel";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label5.Location = new System.Drawing.Point(180, 21);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Enter New User";
+            // 
+            // emailTxt
+            // 
+            this.emailTxt.Location = new System.Drawing.Point(81, 94);
+            this.emailTxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.emailTxt.Name = "emailTxt";
+            this.emailTxt.Size = new System.Drawing.Size(111, 20);
+            this.emailTxt.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(38, 97);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Email: ";
+            // 
+            // passwordTxtBox
+            // 
+            this.passwordTxtBox.Location = new System.Drawing.Point(81, 118);
+            this.passwordTxtBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.passwordTxtBox.Name = "passwordTxtBox";
+            this.passwordTxtBox.PasswordChar = '*';
+            this.passwordTxtBox.Size = new System.Drawing.Size(111, 20);
+            this.passwordTxtBox.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(18, 121);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Password: ";
+            // 
+            // userNameTxt
+            // 
+            this.userNameTxt.Location = new System.Drawing.Point(81, 70);
+            this.userNameTxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.userNameTxt.Name = "userNameTxt";
+            this.userNameTxt.Size = new System.Drawing.Size(111, 20);
+            this.userNameTxt.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 73);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Username: ";
+            // 
+            // PasswordChk
+            // 
+            this.PasswordChk.AutoSize = true;
+            this.PasswordChk.Location = new System.Drawing.Point(81, 142);
+            this.PasswordChk.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.PasswordChk.Name = "PasswordChk";
+            this.PasswordChk.Size = new System.Drawing.Size(102, 17);
+            this.PasswordChk.TabIndex = 6;
+            this.PasswordChk.Text = "Show Password";
+            this.PasswordChk.UseVisualStyleBackColor = true;
+            this.PasswordChk.CheckedChanged += new System.EventHandler(this.PasswordChk_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.emailTxt);
+            this.groupBox1.Controls.Add(this.PasswordChk);
+            this.groupBox1.Controls.Add(this.fNameTxt);
+            this.groupBox1.Controls.Add(this.userNameTxt);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.passwordTxtBox);
+            this.groupBox1.Controls.Add(this.lNameTxt);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Location = new System.Drawing.Point(23, 84);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 167);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "New User Info";
+            // 
+            // DataGrid1
+            // 
+            this.DataGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGrid1.Location = new System.Drawing.Point(254, 92);
+            this.DataGrid1.Name = "DataGrid1";
+            this.DataGrid1.Size = new System.Drawing.Size(237, 159);
+            this.DataGrid1.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 305);
+            this.Controls.Add(this.DataGrid1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.EnterBtn);
+            this.Controls.Add(this.label3);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGrid1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox fNameTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lNameTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button EnterBtn;
+        private System.Windows.Forms.Button ClearBtn;
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox emailTxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox passwordTxtBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox userNameTxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox PasswordChk;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView DataGrid1;
+    }
+}
+
---
+++ Cita450Project/AddUserForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cita450Project
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterBtn_Click(object sender, EventArgs e)
+        {
+            //Pull and save the entered data
+            string fName = fNameTxt.ToString();
+            string lName = lNameTxt.ToString();
+            string userN = userNameTxt.ToString();
+            string email = emailTxt.ToString();
+            string password = passwordTxtBox.ToString();
+
+            //validate the entered data
+
+            //Connect to database to enter data
+            string connectionString;
+            SqlConnection cnn;
+            connectionString = @"Data Source=Cita450.Project.Pankow;
+            Initial Catalog=Users;
+            integrated security info=False;
+                persist security info=False;
+                Trusted_Connection=Yes";
+            cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("SELECT * FROM Customer", cnn);
+
+            //display users entered
+            DataSet dataSet = new DataSet();
+            sqlDataAdapter.Fill(dataSet, "Employees");
+            DataGrid1.DataSource = dataSet.Tables["Employees"].DefaultView;
+            DataGrid1.Show();
+            MessageBox.Show("Connection Open!");
+            cnn.Close();
+        }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            //set all fields to ""
+            fNameTxt.Clear();
+            lNameTxt.Clear();
+            userNameTxt.Clear();
+            emailTxt.Clear();
+            passwordTxtBox.Clear();
+        }
+
+        private void PasswordChk_CheckedChanged(object sender, EventArgs e)
+        {
+            //grab text box property password char
+            char passwordChar = passwordTxtBox.PasswordChar;
+            //convert "*" to char 
+            string str = "*";
+            char character = char.Parse(str);
+            //compare passwordChar to character
+            if (passwordChar == character)
+            {
+                //if yes, change to ""
+                passwordTxtBox.PasswordChar = '\u0000';
+            }
+
+            else
+            {
+                //else remain "*"
+                passwordTxtBox.PasswordChar = character;
+            }
+        }
+
+        private void CancelBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BackBtn_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
