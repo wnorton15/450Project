e4b3fdc0d19199cf27e2eeb6adaec8709a67c616
Liam_Pankow <zzrhydonzz1@gmail.com>
2020-10-10 15:46:30 -0400

    Added: Sign up Button Functionality Clear Button Functionality Login Functionality Username and Password Validation

--- Cita450Project/Login.Designer.cs
+++ Cita450Project/Login.Designer.cs
@@ -31,18 +31,18 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.EnterButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.passwordTxtBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.usernameTxtBox = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.AddUserButton = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.EnterButton = new System.Windows.Forms.Button();
-            this.ClearButton = new System.Windows.Forms.Button();
             this.CancelButton = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.label6 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -71,12 +71,14 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.ClearButton);
             this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.EnterButton);
             this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.passwordTxtBox);
             this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Controls.Add(this.usernameTxtBox);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(75, 117);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(227, 235);
@@ -84,11 +86,86 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Login";
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(28, 47);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(168, 19);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Please fill the fields below:";
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(87, 201);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 23);
+            this.ClearButton.TabIndex = 5;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(89, 155);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(122, 20);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Show password";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // EnterButton
+            // 
+            this.EnterButton.Location = new System.Drawing.Point(6, 201);
+            this.EnterButton.Name = "EnterButton";
+            this.EnterButton.Size = new System.Drawing.Size(75, 23);
+            this.EnterButton.TabIndex = 4;
+            this.EnterButton.Text = "Enter";
+            this.EnterButton.UseVisualStyleBackColor = true;
+            this.EnterButton.Click += new System.EventHandler(this.EnterButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 15);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Password: ";
+            // 
+            // passwordTxtBox
+            // 
+            this.passwordTxtBox.Location = new System.Drawing.Point(89, 127);
+            this.passwordTxtBox.Name = "passwordTxtBox";
+            this.passwordTxtBox.Size = new System.Drawing.Size(132, 22);
+            this.passwordTxtBox.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Username: ";
+            // 
+            // usernameTxtBox
+            // 
+            this.usernameTxtBox.Location = new System.Drawing.Point(89, 99);
+            this.usernameTxtBox.Name = "usernameTxtBox";
+            this.usernameTxtBox.Size = new System.Drawing.Size(132, 22);
+            this.usernameTxtBox.TabIndex = 0;
+            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.AddUserButton);
             this.groupBox2.Controls.Add(this.label3);
-            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.Location = new System.Drawing.Point(368, 117);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(227, 235);
@@ -103,8 +180,9 @@
             this.AddUserButton.Name = "AddUserButton";
             this.AddUserButton.Size = new System.Drawing.Size(109, 23);
             this.AddUserButton.TabIndex = 1;
-            this.AddUserButton.Text = "Add User Page";
+            this.AddUserButton.Text = "Sign-up Page";
             this.AddUserButton.UseVisualStyleBackColor = true;
+            this.AddUserButton.Click += new System.EventHandler(this.AddUserButton_Click);
             // 
             // label3
             // 
@@ -117,86 +195,14 @@
             this.label3.Text = "If you do not have an account \r\nPlease click the button below\r\nto take you to the" +
     " Add User\r\nPage.";
             // 
-            // EnterButton
-            // 
-            this.EnterButton.Location = new System.Drawing.Point(12, 394);
-            this.EnterButton.Name = "EnterButton";
-            this.EnterButton.Size = new System.Drawing.Size(75, 23);
-            this.EnterButton.TabIndex = 4;
-            this.EnterButton.Text = "Enter";
-            this.EnterButton.UseVisualStyleBackColor = true;
-            // 
-            // ClearButton
-            // 
-            this.ClearButton.Location = new System.Drawing.Point(94, 394);
-            this.ClearButton.Name = "ClearButton";
-            this.ClearButton.Size = new System.Drawing.Size(75, 23);
-            this.ClearButton.TabIndex = 5;
-            this.ClearButton.Text = "Clear";
-            this.ClearButton.UseVisualStyleBackColor = true;
-            // 
             // CancelButton
             // 
-            this.CancelButton.Location = new System.Drawing.Point(176, 394);
+            this.CancelButton.Location = new System.Drawing.Point(12, 394);
             this.CancelButton.Name = "CancelButton";
             this.CancelButton.Size = new System.Drawing.Size(75, 23);
             this.CancelButton.TabIndex = 6;
             this.CancelButton.Text = "Cancel";
             this.CancelButton.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(89, 119);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(132, 22);
-            this.textBox1.TabIndex = 0;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(6, 122);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(81, 15);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Username: ";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(89, 147);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(132, 22);
-            this.textBox2.TabIndex = 2;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(6, 150);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(77, 15);
-            this.label5.TabIndex = 3;
-            this.label5.Text = "Password: ";
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(89, 175);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(135, 20);
-            this.checkBox1.TabIndex = 4;
-            this.checkBox1.Text = "Show password";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(28, 49);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(168, 19);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "Please fill the fields below:";
             // 
             // Login
             // 
@@ -204,8 +210,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(670, 429);
             this.Controls.Add(this.CancelButton);
-            this.Controls.Add(this.ClearButton);
-            this.Controls.Add(this.EnterButton);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label2);
@@ -235,8 +239,8 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox passwordTxtBox;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox usernameTxtBox;
     }
 }
--- Cita450Project/Login.cs
+++ Cita450Project/Login.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +18,128 @@ namespace Cita450Project
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            //grab text box property password char
+            char passwordChar = passwordTxtBox.PasswordChar;
+            //convert "*" to char 
+            string str = "*";
+            char character = char.Parse(str);
+            //compare passwordChar to character
+            if (passwordChar == character)
+            {
+                //if yes, change to ""
+                passwordTxtBox.PasswordChar = '\u0000';
+            }
+
+            else
+            {
+                //else remain "*"
+                passwordTxtBox.PasswordChar = character;
+            }
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            if (usernameTxtBox.Text == "")
+            {
+                MessageBox.Show("Please enter username", "Error");
+                usernameTxtBox.Focus();
+                return;
+            }
+            if (passwordTxtBox.Text == "")
+            {
+                MessageBox.Show("Please enter password", "Error");
+                passwordTxtBox.Focus();
+                return;
+            }
+            try
+            {
+
+                //Connect to database to enter data
+                string connectionString;
+
+                SqlCommand myCommand = default(SqlCommand);
+
+                SqlConnection cnn;
+
+                //Change "Data Source" field to system name
+                connectionString = @"Data Source=MAC18552;
+                Initial Catalog=IceDB;
+                integrated security=False;
+                persist security info=False;
+                Trusted_Connection=Yes";
+                cnn = new SqlConnection(connectionString);
+
+                //create query to enter into database
+                //ask database if entered username and password exist
+                string query = "SELECT Username,Password " +
+                                "From Users WHERE" +
+                                "Username = @Username AND" +
+                                "Password = @Password";
+
+                //Create new SQL parameter
+                SqlParameter uName = new SqlParameter("@Username", SqlDbType.VarChar);
+                SqlParameter uPassword = new SqlParameter("@Password", SqlDbType.VarChar);
+
+                //take user entered data
+                uName.Value = usernameTxtBox.Text;
+                uPassword.Value = passwordTxtBox.Text;
+
+                //replace @Username,@Password in query with User entered data
+                myCommand.Parameters.Add(uName);
+                myCommand.Parameters.Add(uPassword);
+
+                SqlCommand cmd = new SqlCommand(query, cnn);
+
+                cnn.Open();
+
+                SqlDataReader myReader = myCommand.ExecuteReader(CommandBehavior.CloseConnection);
+
+                if (myReader.Read() == true)
+                {
+                    MessageBox.Show("Login Successful, Welcome " + usernameTxtBox.Text);
+                    //Take user to main menu
+                    Clearfields();
+                }
+
+                else
+                {
+                    MessageBox.Show("Login Error!!!", "Please check Username and Password!");
+                    Clearfields();
+                }
+                if(cnn.State == ConnectionState.Open)
+                {
+                    cnn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            Clearfields();
+        }
+
+        private void Clearfields()
+        {
+            //set all fields to ""
+            usernameTxtBox.Clear();
+            passwordTxtBox.Clear();
+        }
+
+        private void AddUserButton_Click(object sender, EventArgs e)
+        {
+            Form1 addUser = new Form1();
+            addUser.Show();
         }
     }
 }
